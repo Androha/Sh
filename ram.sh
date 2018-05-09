68ea7603b99ebf846bffa648070c4154783af239
Androha <androha@mail.ru>
2018-05-09 21:31:36 +0300

    src.txt

--- ram.sh
+++ ram.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#apt install bc
+#apt install bc # You need to install calc first
+names=false
 all=false
 file=""
 list=""
@@ -8,31 +9,32 @@ while [ -n "$1" ]
 do
   case "$1" in
     -a) all=true;; # Delete all
-    -f) file="$2" #Get source from a file
+    -f) file="$2" # Get source from a file
         shift ;;
-    -l) list+="$2 " #Get source from a list
+    -l) list+="$2 " # Get source from a list
         shift;;
-    --req) req="$2" #Required amount of memory
+    -n) names=true;; # Use process names instead of PIDs
+    --req) req="$2" # Required amount of memory
            shift;;
     *) echo "$1 is not an option";;
   esac
   shift
 done
 
-mul=$(echo "$req" | sed 's/[^a-zA-Z]*//g') #Getting symbolic multiplyer
-req=$(echo "$req" | sed 's/[^0-9.]*//g') #Getting numeric value of required amount
+mul=$(echo "$req" | sed 's/[^a-zA-Z]*//g') # Getting symbolic multiplyer
+req=$(echo "$req" | sed 's/[^0-9.]*//g') # Getting numeric value of required amount
 
 if [ "$req" != "" ]
 then
   case "$mul" in
-    M) req=$(echo "$req*1000" | bc)
+    M) req=$(echo "$req*1000" | bc) # Megabytes
        req=$(echo "($req+0.5)/1" | bc);;
-    G) req=$(echo "$req*1000000" | bc)
+    G) req=$(echo "$req*1000000" | bc) # Gigabytes
        req=$(echo "($req+0.5)/1" | bc);;
-    *) req=$(echo "($req+0.5)/1" | bc);;
+    *) req=$(echo "($req+0.5)/1" | bc);; # Ignore other multiplyers
   esac
 
-  if [ -f "$file" ]
+  if [ -f "$file" ] # Add file contents to the list of processes
   then
     while read -r line
     do
@@ -46,8 +48,18 @@ then
     for prc in $list
     do
       #Workfield
-      echo "$prc"
+      free=$(cat /proc/meminfo | grep MemFree | grep -Eo '[0-9]+' | tail -n 1)
+      if [[ $all == "false" && $free > $req ]]; then echo "Requirements are met"; exit 0; fi # Break if there is enough memory and
+      if [[ $names == "false" ]]                                                             # you don't need to kill all processes
+      then
+        kill $prc # Option for PIDs
+        if [ $? -eq 0 ]; then echo "Process $prc killed"; else echo "Failed to kill $prc"; fi
+      else
+        pkill $prc # Option for process names
+        if [ $? -eq 0 ]; then echo "Process $prc killed"; else echo "Failed to kill $prc"; fi
+      fi
     done
+    exit 0;
   else
     echo "You must enter at least one process to be killed.
 It can be done via -f or -l options."
